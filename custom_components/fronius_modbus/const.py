7a9904c5d23201bc9b3462937f608c3f986bbbad
callifo <38901518+callifo@users.noreply.github.com>
2025-11-07 16:06:10 +1030

    read isolation resistance

--- custom_components/fronius_modbus/const.py
+++ custom_components/fronius_modbus/const.py
@@ -72,7 +72,8 @@ INVERTER_SENSOR_TYPES = {
     'OutPFSet_Ena': ['Fixed power factor', 'OutPFSet_Ena', None, None, None, None, EntityCategory.DIAGNOSTIC],
     'VArPct_Ena': ['Limit VAr control', 'VArPct_Ena', None, None, None, None, EntityCategory.DIAGNOSTIC],
     'PhVphA': ['AC voltage L1-N', 'PhVphA', SensorDeviceClass.VOLTAGE, SensorStateClass.MEASUREMENT, 'V', 'mdi:lightning-bolt', None],
-    'unit_id': ['Modbus ID', 'i_unit_id', None, None, None, None, EntityCategory.DIAGNOSTIC],    
+    'unit_id': ['Modbus ID', 'i_unit_id', None, None, None, None, EntityCategory.DIAGNOSTIC],
+    'isolation_resistance': ['Isolation Resistance', 'isolation_resistance', None, SensorStateClass.MEASUREMENT, 'MΩ', 'mdi:omega', None],
 }
 
 INVERTER_SYMO_SENSOR_TYPES = {
